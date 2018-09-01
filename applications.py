faf50a689b1bfa2563305f9145d5520b48612605
wongjyusing <wongjyusing@gmail.com>
2018-09-01 13:08:20 +0800

    添加了使用说明

--- applications.py
+++ applications.py
@@ -10,12 +10,4 @@ class NewApp(Application):
         print('服务器程序启动成功')
         print(f'请打开浏览器输入 http://127.0.0.1:{port_num}')
         print('按下 Ctrl + C 退出服务器程序')
-        Application.__init__(self, handlers,**settings)  # , debug=True)
-
-'''
-
-client = pymongo.MongoClient(host='127.0.0.1', port=27017)
-#db_auth = client.admin
-#db_auth.authenticate('test_user', '123456')
-self.db = client['runoob']
-'''
+        Application.__init__(self, handlers,**settings)
